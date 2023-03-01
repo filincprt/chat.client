6ad12323c0596a9b5790501da38073c2b865c729
vital <vital@FILINPC>
2023-03-01 08:56:05 +0500

    Добавьте файлы проекта.

---
+++ chat.client/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace chat.client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите IP-адрес сервера:");
+            string serverIp = Console.ReadLine();
+            Console.WriteLine("Введите порт сервера:");
+            int serverPort = int.Parse(Console.ReadLine());
+
+            TcpClient client = new TcpClient(serverIp, serverPort);
+            NetworkStream stream = client.GetStream();
+
+            Console.WriteLine("Введите ваше имя:");
+            string userName = Console.ReadLine();
+            byte[] userNameBytes = Encoding.UTF8.GetBytes(userName);
+            stream.Write(userNameBytes, 0, userNameBytes.Length);
+
+            Console.WriteLine("Добро пожаловать в чат!");
+            Console.WriteLine("Введите сообщения:");
+
+            while (true)
+            {
+                string message = Console.ReadLine();
+                byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+                stream.Write(messageBytes, 0, messageBytes.Length);
+            }
+        }
+    }
+}
---
+++ chat.client/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("chat.client")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("chat.client")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("2ff75501-2c6c-441d-af4c-1669fe42fbc4")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

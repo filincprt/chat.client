0797d4f4bf5c15c8ee45c553624af9bd4a88e638
Филин Виталий <vitalyfilin19@gmail.com>
2023-03-01 09:24:24 +0500

    fix 1

--- chat.client/Program.cs
+++ chat.client/Program.cs
@@ -2,12 +2,13 @@
 using System.IO;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace chat.client
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Введите IP-адрес сервера:");
             string serverIp = Console.ReadLine();
@@ -20,16 +21,29 @@ namespace chat.client
             Console.WriteLine("Введите ваше имя:");
             string userName = Console.ReadLine();
             byte[] userNameBytes = Encoding.UTF8.GetBytes(userName);
-            stream.Write(userNameBytes, 0, userNameBytes.Length);
+            await stream.WriteAsync(userNameBytes, 0, userNameBytes.Length);
 
             Console.WriteLine("Добро пожаловать в чат!");
             Console.WriteLine("Введите сообщения:");
 
+            // Асинхронное чтение данных из сетевого потока
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    byte[] buffer = new byte[4096];
+                    int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+                    string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    Console.WriteLine(message);
+                }
+            });
+
+            // Отправка сообщений на сервер
             while (true)
             {
                 string message = Console.ReadLine();
                 byte[] messageBytes = Encoding.UTF8.GetBytes(message);
-                stream.Write(messageBytes, 0, messageBytes.Length);
+                await stream.WriteAsync(messageBytes, 0, messageBytes.Length);
             }
         }
     }
